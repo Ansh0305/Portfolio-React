2125dd07fa7c2aedac8246dec67f72bb73cbb1f6
Ansh0305 <saiansh2016@gmail.com>
2025-10-01 01:59:34 +0530

    Added EmailJS-com to receive emails

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import { RevealOnScroll } from "../RevealOnScroll";
+import emailjs from 'emailjs-com';
 
 export const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const SERVICE_ID = "service_8075qlj";
+    const TEMPLATE_ID = "template_6twau4l";
+    const PUBLIC_KEY = "ODgjoIPRVSq7RJ9Ar";
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
+            .then((result) => {
+                alert("Message Sent!");
+                setFormData({ name: "", email: "", message: "" });
+            }).catch(() => alert("Oops! Something went wrong. Please try again."));
+    };
+
     return (
         <section
             id="contact"
@@ -11,35 +32,46 @@ export const Contact = () => {
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
                         Get In Touch
                     </h2>
-                    <form action="" className="space-y-6">
+                    <form className="space-y-6" onSubmit={handleSubmit}>
                         <div className="relative">
                             <input
                                 type="text"
                                 id="name"
                                 name="name"
                                 required
+                                value={formData.name}
                                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                                 placeholder="Name"
-
+                                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
                             />
+                        </div>
+                        <div className="relative">
                             <input
                                 type="email"
                                 id="email"
                                 name="email"
                                 required
+                                value={formData.email}
                                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                                 placeholder="example@gmail.com"
+                                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                             />
+                        </div>
+                        <div className="relative">
                             <textarea
-                                id="email"
+                                id="message"
                                 name="message"
                                 rows={5}
                                 required
+                                value={formData.message}
                                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
                                 placeholder="Your Message.."
+                                onChange={(e) => setFormData({ ...formData, message: e.target.value })}
                             />
                         </div>
-                        <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover: shadow-[0_0_15px_rgba(59, 130,246, 0.4)]">Send Message</button>
+                        <button type="submit" className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
+                            Send Message
+                        </button>
                     </form>
                 </div>
             </RevealOnScroll>
