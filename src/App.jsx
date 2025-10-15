4bf86b1978126d8374ad8dc2fc43ed1d716646b1
Ansh0305 <saiansh2016@gmail.com>
2025-10-15 22:07:27 +0530

    Added footer and changed the icon

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Home } from './components/sections/Home';
 import { About } from './components/sections/About';
 import { Projects } from './components/sections/Projects';
 import { Contact } from './components/sections/Contact';
+import { Footer } from './Footer';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false)
@@ -23,6 +24,7 @@ function App() {
       <About/>
       <Projects/>
       <Contact/>
+      <Footer/>
 
     </div>
     </>
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+    return (
+        <footer className="w-full py-4 bg-black text-white text-center text-sm font-light tracking-wide border-t border-gray-800">
+            Created by Sirigiri Sai Ansh Raj 💀
+        </footer>
+
+    )
+}
