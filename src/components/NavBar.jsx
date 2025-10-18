a6eed551f5fc006ca0278f4c14c94a51620494df
Ansh0305 <saiansh2016@gmail.com>
2025-10-19 00:44:49 +0530

    Updated Resume

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -47,7 +47,7 @@ export const NavBar = ({menuOpen, setMenuOpen}) => {
               Contact
             </a>
             <a
-              href="/Resume.pdf"
+              href="/Sirigiri Sai Ansh Raj.pdf"
               className=" bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover: -translate-y-0.5 hover: shadow-[0_0_15px_rgba(59, 130,246, 0.4)]"
               target="_blank"
             >
