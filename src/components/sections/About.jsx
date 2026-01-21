c142fd5780ed2198dbe409e450e8a300e2ed406f
Ansh0305 <saiansh2016@gmail.com>
2026-01-21 22:17:13 +0530

    Solved card stretching issue by setting h-max

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -68,7 +68,7 @@ export const About = () => {
                     </div>
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-                        <div className="p-6 rounded-xl border-white/10 border hover:translate-y-1 transition-all">
+                        <div className="p-6 rounded-xl border-white/10 border hover:translate-y-1 transition-all h-max">
                             <h3 className="text-xl font-bold mb-4">🏫 Education</h3>
                             <ul className="list-disc list-inside text-gray-300 space-y-2">
                                 <li>
@@ -88,7 +88,7 @@ export const About = () => {
                                 <li>Relevant courses: <strong>Data Structures & Algorithms, Database Management, System Design, OOPS</strong> </li>
                             </ul>
                         </div>
-                        <div className="p-6 rounded-xl border-white/10 border hover:translate-y-1 transition-all">
+                        <div className="p-6 rounded-xl border-white/10 border hover:translate-y-1 transition-all h-max">
                             <h3 className="text-xl font-bold mb-4">🏫 Work Experience</h3>
                             <div className="space-y-4 text-gray-300">
                                 <div>
