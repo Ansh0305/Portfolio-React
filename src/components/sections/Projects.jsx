8e9d87528695d2d97fdb7d99d15527da52f22eb3
Ansh0305 <saiansh2016@gmail.com>
2026-01-25 19:44:53 +0530

    Project update

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -52,7 +52,7 @@ export const Projects = () => {
                         <h3 className="text-xl font-bold mb-2">AI-EmailGenerator</h3>
                         <p className="text-gray-400 mb-4">The AI-EmailGenerator is a powerful, intelligent web application designed to streamline and automate the process of writing professional emails. Built with React and bundled using Vite for a fast development experience and dynamic user interface, it leverages cutting-edge Large Language Models (LLMs) to generate email drafts from simple user prompts.</p>
                         <div className="flex flex-wrap gap-2 mb-4">
-                            {["React", "Node.js", "TailwindCSS"].map((skills, key) => (
+                            {["React", "Node.js", "TailwindCSS"," OpenAI-API"].map((skills, key) => (
                                 <span
                                     key={key}
                                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
@@ -62,14 +62,16 @@ export const Projects = () => {
                         </div>
 
                         <div className="flex justify-between items-center">
-                            <a href="https://github.com/Ansh0305/AI-EmailGenerator" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Code →</a>
+                            <a href="https://github.com/Ansh0305/AI-EmailGenerator" target="_blank" rel="noopener noreferrer" 
+                            className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Code →</a>
+                            <a href="https://ai-emailgenerator.netlify.app/" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View project →</a>
                         </div>
                     </div>
 
                     <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                        <h3 className="text-xl font-bold mb-2">Spotify Clone</h3>
-                        <p className="text-gray-400 mb-4">This project is a Spotify-inspired music streaming clone developed using only HTML, CSS, and vanilla JavaScript. It replicates the core interface and functionalities of the Spotify platform while operating entirely in the browser. All songs are stored locally, enabling fast playback without external APIs or databases.</p>
-                        <div className="flex flex-wrap gap-2 mb-4">
+                        <h3 className="text-xl font-bold mb-2">Buyza</h3>
+                        <p className="text-gray-400 mb-4">Buyza is a full-stack e-commerce web application designed to deliver a smooth and user-friendly online shopping experience. It features product browsing, filtering, cart management, and secure checkout with an emphasis on clean UI, performance, and scalability. The project demonstrates modern web development practices and a focus on usability and real-world e-commerce functionality.</p>
+                        {/* <div className="flex flex-wrap gap-2 mb-4">
                             {["HTML", "CSS", "JavaScript"].map((skills, key) => (
                                 <span
                                     key={key}
@@ -77,10 +79,10 @@ export const Projects = () => {
                                     {skills}
                                 </span>
                             ))}
-                        </div>
+                        </div> */}
 
                         <div className="flex justify-between items-center">
-                            <a href="https://github.com/Ansh0305/Projects/tree/main/Spotify" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Code →</a>
+                            <a href="https://github.com/Ansh0305/Buyza" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-300 transition-colors my-4">Under Construction🚧</a>
                         </div>
                     </div>
 
