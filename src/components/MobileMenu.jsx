630ab8e0eab94d9f87fd964d17413dcf0858daff
Ansh0305 <saiansh2016@gmail.com>
2026-01-25 21:26:13 +0530

    Updated Resume

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -62,14 +62,12 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
                 contact
             </a>
             <a
-                href="/Sirigiri Sai Ansh Raj Resume.pdf"
+                href="/SirigiriAnsh.pdf"
                 className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
                 target="_blank"
             >
                 Download CV
             </a>
-
-
         </div>
     );
 };
