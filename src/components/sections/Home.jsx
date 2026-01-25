d308c9294cf085466501398fff8a89ea0cda90e1
Ansh0305 <saiansh2016@gmail.com>
2026-01-26 00:37:00 +0530

    Added instagram icon and linked it to insta

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -57,6 +57,17 @@ export const Home = () => {
                             </svg>
                         </a>
                         <a
+                            href="https://www.instagram.com/saiansh_sirigiri/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-gray-400 hover:text-blue-500 transition-colors"
+                            aria-label="Instagram"
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram-icon lucide-instagram">
+                                <rect width="20" height="20" x="2" y="2" rx="5" ry="5" /><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" /><line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+                            </svg>
+                        </a>
+                        <a
                             href="mailto:saiansh2016@gmail.com"
                             className="text-gray-400 hover:text-cyan-400 transition-colors"
                             aria-label="Email"
