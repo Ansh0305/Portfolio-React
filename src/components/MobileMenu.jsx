949dcb11e6a33d8a0a16d510b32eadcd13097b66
Ansh0305 <saiansh2016@gmail.com>
2026-01-25 21:30:49 +0530

    Resume Update

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -62,7 +62,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
                 contact
             </a>
             <a
-                href="/SirigiriAnsh.pdf"
+                href="/SirigiriAnshResume.pdf"
                 className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
                 target="_blank"
             >
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -47,7 +47,7 @@ export const NavBar = ({ menuOpen, setMenuOpen }) => {
               Contact
             </a>
             <a
-              href="/SirigiriAnsh.pdf"
+              href="/SirigiriAnshResume.pdf"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
               target="_blank"
               rel="noopener noreferrer"
